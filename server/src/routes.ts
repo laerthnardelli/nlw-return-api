0e087065751e69843f40edbb910a75acb771e0f3
laerthnardelli <laerthnardelli@gmail.com>
2022-05-05 14:03:45 -0300

    changed to use routes

---
+++ server/src/routes.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import { prisma } from "./prisma";
+
+export const routes = express.Router();
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "46129e23098a1b",
+    pass: "5894054a61dd85",
+  },
+});
+
+//http://localhost:3333/feedbacks
+routes.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedgat.com>",
+    to: "Laérth Nardelli <laerthnardelli@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
+  });
+
+  return res.status(201).json({ data: feedback });
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,46 +1,11 @@
 import express from "express";
-import nodemailer from "nodemailer";
-import { prisma } from "./prisma";
+
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "46129e23098a1b",
-    pass: "5894054a61dd85",
-  },
-});
-
-//http://localhost:3333/feedbacks
-app.post("/feedbacks", async (req, res) => {
-  const { type, comment, screenshot } = req.body;
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
-  });
-
-  await transport.sendMail({
-    from: "Equipe Feedget <oi@feedgat.com>",
-    to: "Laérth Nardelli <laerthnardelli@gmail.com>",
-    subject: "Novo feedback",
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`,
-    ].join("\n"),
-  });
-
-  return res.status(201).json({ data: feedback });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("HTTP server running!");
