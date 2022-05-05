12349c4daf1076ddc487c4037770be8e808d6cfd
laerthnardelli <laerthnardelli@gmail.com>
2022-05-05 11:23:40 -0300

    created feedback route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,24 @@
 import express from "express";
+import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/feedbacks", (req, res) => {
-  console.log(req.body);
-  return res.send("Hello World");
-});
 //http://localhost:3333/feedbacks
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
+});
 
 app.listen(3333, () => {
   console.log("HTTP server running!");
