a000fc156dcddc31ce93183324cb8134bb0acfce
laerthnardelli <laerthnardelli@gmail.com>
2022-05-05 11:12:27 -0300

    test to use Insomnia

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,13 @@ import express from "express";
 
 const app = express();
 
-//GET = Buscar informações
-//POST = Cadastrar informações
-//PATCH = Atalizar uma informação única de uma entidade
-//DELETE = Deletar uma informção
+app.use(express.json());
 
-app.get("/users", (req, res) => {
+app.post("/feedbacks", (req, res) => {
+  console.log(req.body);
   return res.send("Hello World");
 });
-//http://localhost:3333/users
+//http://localhost:3333/feedbacks
 
 app.listen(3333, () => {
   console.log("HTTP server running!");
