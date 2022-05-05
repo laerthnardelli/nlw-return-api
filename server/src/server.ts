167d989e97197ee4bdf28ff4433cd6293f5a7bd7
laerthnardelli <laerthnardelli@gmail.com>
2022-05-04 21:04:33 -0300

    example of using express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-const teste = 1;
+import express from "express";
+
+const app = express();
+
+app.get("/users", (req, res) => {
+  return res.send("Hello World");
+});
+//http://localhost:3333/users
+
+app.listen(3333, () => {
+  console.log("HTTP server running!");
+});
