27df06644ed929e389015db0367e83107962e3db
laerthnardelli <laerthnardelli@gmail.com>
2022-05-05 10:57:01 -0300

    methods http

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+//GET = Buscar informações
+//POST = Cadastrar informações
+//PATCH = Atalizar uma informação única de uma entidade
+//DELETE = Deletar uma informção
+
 app.get("/users", (req, res) => {
   return res.send("Hello World");
 });
