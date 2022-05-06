db1475e10eb3711b5acf0f6263d1fe09e9d82ffd
laerthnardelli <laerthnardelli@gmail.com>
2022-05-05 22:09:28 -0300

    added test - should be able to submit feedback

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,3 +1,18 @@
-test('sum 2 + 2', () => {
-  expect(2 + 2).toBe(4)
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: async () => {} },
+  { sendMail: async () => {} }
+);
+
+describe("Submit feedback", () => {
+  it("should be able to submit feedback", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "exemple comment",
+        screenshot: "data:image/png;base64,jsdjh23Laerthck9ww9d9",
+      })
+    ).resolves.not.toThrow();
+  });
 });
